977bed75f0a015ddd727c343b87912bff436865a
Alexander Knyazev <AlexandrKnyazev1395@mail.ru>
2018-03-19 15:45:47 +0300

    added rule for no-trailing-spaces

--- .eslintrc.js
+++ .eslintrc.js
@@ -3,6 +3,7 @@ module.exports = {
     "rules": {
         "no-console": "off",
         "no-debugger": "off",
-        "no-unused-vars": "off"
+        "no-unused-vars": "off",
+        "no-trailing-spaces": [true, "ignore-jsdoc", "ignore-jsdoc", "ignore-blank-lines"],
     }
 };
