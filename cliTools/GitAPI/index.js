0fbca033a6fad027f33bb9823c1993190d639111
Alexander Knyazev <AlexandrKnyazev1395@mail.ru>
2018-03-19 13:16:07 +0300

    basic express template

---
+++ cliTools/GitAPI/index.js
@@ -0,0 +1,13 @@
+const fs = require('fs');
+
+
+class GitAPI {
+  constructor() {
+
+  }
+  getFiles() {
+    console.log("files")
+  }
+}
+
+module.exports = GitAPI;
---
+++ routes/index.js
@@ -0,0 +1,14 @@
+const express = require('express');
+const router = express.Router();
+
+const GitAPI = require('../cliTools/GitAPI')
+
+/* GET home page. */
+router.get('/', function(req, res, next) {
+  const gitAPI = new GitAPI();
+  gitAPI.getFiles();
+
+  res.render('index', { title: 'GitPlace' });
+});
+
+module.exports = router;
