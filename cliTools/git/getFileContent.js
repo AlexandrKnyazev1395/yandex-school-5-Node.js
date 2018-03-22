d68c3f266163546d86629bf00163104345978bb1
Alexander Knyazev <AlexandrKnyazev1395@mail.ru>
2018-03-22 12:12:05 +0300

    deleted unnecessary function

--- cliTools/git/getFileContent.js
+++ cliTools/git/getFileContent.js
@@ -4,10 +4,6 @@ async function getFileContent(destination, path) {
   const bashText = `git show ${destination}:${path}`;
   const executeCliOptions = { isSplitByEnter: false };
   const fileContent = await executeCliCommand(bashText, executeCliOptions);
-  if (!fileContent.errors.length) {
-    fileContent.body = fileContent.body.join('/n');
-    fileContent.body = decodeURIComponent(fileContent.body);
-  }
   return fileContent;
 }
 
