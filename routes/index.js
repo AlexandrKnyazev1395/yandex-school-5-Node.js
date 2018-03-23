cab8e4e0b10fd99580b47e65360fe851087d2e23
Alexander Knyazev <AlexandrKnyazev1395@mail.ru>
2018-03-23 12:24:19 +0300

    clone repo after deploying

--- routes/index.js
+++ routes/index.js
@@ -5,19 +5,18 @@ const router = express.Router();
 const { getBranches } = require('../cliTools/git');
 
 router.get('/', async (req, res) => {
-  //const branches = await getBranches();
-  //if (!branches.errors.length) {
+  const branches = await getBranches();
+  if (!branches.errors.length) {
     res.render('index', {
       title: 'Мой гит',
       section: 'Ветки',
-      //branches: branches.body,
-      branches: [2, 3, 4],
+      branches: branches.body,
     });
-  /*} else {
+  } else {
     res.render('error', {
       errorText: branches.errors.join(','),
     });
-  }*/
+  }
 });
 
 module.exports = router;
